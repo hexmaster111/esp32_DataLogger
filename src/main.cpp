31e45136b2d661086f16d8465ff114deb0e60448
Hailey <Hexmaster111@gmail.com>
2021-05-01 10:53:27 -0500

    Clean up and added a debug compiler var

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,9 @@ NOTES:
 
 #define VERSION_NUMBER 1.0 //just so i can know on the display
 
+//Uncomment to enable debug messages
+#define DEBUG
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
@@ -32,7 +35,10 @@ NOTES:
 #define OLED_RESET 99
 
 #define GPSport_h
+
+#ifdef DEBUG
 #define DEBUG_PORT Serial
+#endif
 
 #define gpsPort GPS_Serial //was GPS_Serial(1)
 #define GPS_PORT_NAME "GPS_Serial 1"
@@ -169,107 +175,32 @@ void adjustTime(NeoGPS::time_t &dt) //We only use this for the display (24 hr ti
   dt = seconds; // convert seconds back to a date/time structure
 }
 
-void listDir(fs::FS &fs, const char *dirname, uint8_t levels) //unused currently
-{
-  DEBUG_PORT.printf("Listing directory: %s\n", dirname);
-
-  File root = fs.open(dirname);
-  if (!root)
-  {
-    DEBUG_PORT.println("Failed to open directory");
-    return;
-  }
-  if (!root.isDirectory())
-  {
-    DEBUG_PORT.println("Not a directory");
-    return;
-  }
-
-  File file = root.openNextFile();
-  while (file)
-  {
-    if (file.isDirectory())
-    {
-      DEBUG_PORT.print(" DIR : ");
-      DEBUG_PORT.println(file.name());
-      if (levels)
-      {
-        listDir(fs, file.name(), levels - 1);
-      }
-    }
-    else
-    {
-      DEBUG_PORT.print(" FILE: ");
-      DEBUG_PORT.print(file.name());
-      DEBUG_PORT.print(" SIZE: ");
-      DEBUG_PORT.println(file.size());
-    }
-    file = root.openNextFile();
-  }
-}
-
-void createDir(fs::FS &fs, const char *path) //unused currently
-{
-  DEBUG_PORT.printf("Creating Dir: %s\n", path);
-  if (fs.mkdir(path))
-  {
-    DEBUG_PORT.println("Dir created");
-  }
-  else
-  {
-    DEBUG_PORT.println("mkdir failed");
-  }
-}
-
-void removeDir(fs::FS &fs, const char *path) //unused currently
-{
-  DEBUG_PORT.printf("Removing Dir: %s\n", path);
-  if (fs.rmdir(path))
-  {
-    DEBUG_PORT.println("Dir removed");
-  }
-  else
-  {
-    DEBUG_PORT.println("rmdir failed");
-  }
-}
-
-void readFile(fs::FS &fs, const char *path) //unused currently
-{
-  DEBUG_PORT.printf("Reading file: %s\n", path);
-
-  File file = fs.open(path);
-  if (!file)
-  {
-    DEBUG_PORT.println("Failed to open file for reading");
-    return;
-  }
-
-  DEBUG_PORT.print("Read from file: ");
-  while (file.available())
-  {
-    DEBUG_PORT.write(file.read());
-  }
-}
-
 void writeFile(fs::FS &fs, String path, const char *message) //used for the inital file write
 {
+#ifdef DEBUG
   DEBUG_PORT.print("Writing file: ");
   DEBUG_PORT.println(String(path));
+#endif
 
   File file = fs.open(path, FILE_WRITE);
   if (!file)
   {
+#ifdef DEBUG
     DEBUG_PORT.println("Failed to open file for writing");
+#endif
     return;
   }
   if (file.print(message))
   {
+#ifdef DEBUG
     DEBUG_PORT.println("File written");
+#endif
   }
   else
   {
+#ifdef DEBUG
     DEBUG_PORT.println("Write failed");
+#endif
   }
 }
 
@@ -277,100 +208,33 @@ bool blink = true;
 
 void appendFile(fs::FS &fs, String path, String message) //appends data to our file
 {
+#ifdef DEBUG
   DEBUG_PORT.print("Appending to file:");
   DEBUG_PORT.println(path);
+#endif
 
   File file = fs.open(path, FILE_APPEND);
   if (!file)
   {
+#ifdef DEBUG
     DEBUG_PORT.println("Failed to open file for appending");
+#endif
     return;
   }
   if (file.print(message))
   {
+#ifdef DEBUG
     DEBUG_PORT.println("Message appended");
+#endif
     digitalWrite(LED_BUILTIN, blink);
     blink = !blink;
   }
   else
   {
+#ifdef DEBUG
     DEBUG_PORT.println("Append failed");
+#endif
   }
-}
-
-void renameFile(fs::FS &fs, const char *path1, const char *path2) //unused currently
-{
-  DEBUG_PORT.printf("Renaming file %s to %s\n", path1, path2);
-  if (fs.rename(path1, path2))
-  {
-    DEBUG_PORT.println("File renamed");
-  }
-  else
-  {
-    DEBUG_PORT.println("Rename failed");
-  }
-}
-
-void deleteFile(fs::FS &fs, const char *path) //unused currently
-{
-  DEBUG_PORT.printf("Deleting file: %s\n", path);
-  if (fs.remove(path))
-  {
-    DEBUG_PORT.println("File deleted");
-  }
-  else
-  {
-    DEBUG_PORT.println("Delete failed");
-  }
-}
-
-void testFileIO(fs::FS &fs, const char *path) //unused currently
-{
-  File file = fs.open(path);
-  static uint8_t buf[512];
-  size_t len = 0;
-  uint32_t start = millis();
-  uint32_t end = start;
-  if (file)
-  {
-    len = file.size();
-    size_t flen = len;
-    start = millis();
-    while (len)
-    {
-      size_t toRead = len;
-      if (toRead > 512)
-      {
-        toRead = 512;
-      }
-      file.read(buf, toRead);
-      len -= toRead;
-    }
-    end = millis() - start;
-    DEBUG_PORT.printf("%u bytes read for %u ms\n", flen, end);
-    file.close();
-  }
-  else
-  {
-    DEBUG_PORT.println("Failed to open file for reading");
-  }
-
-  file = fs.open(path, FILE_WRITE);
-  if (!file)
-  {
-    DEBUG_PORT.println("Failed to open file for writing");
-    return;
-  }
-
-  size_t i;
-  start = millis();
-  for (i = 0; i < 2048; i++)
-  {
-    file.write(buf, 512);
-  }
-  end = millis() - start;
-  DEBUG_PORT.printf("%u bytes written for %u ms\n", 2048 * 512, end);
-  file.close();
 }
 
 /*
@@ -409,16 +273,18 @@ void setup()
 
   if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
   {
+#ifdef DEBUG
     DEBUG_PORT.println(F("SSD1306 allocation failed"));
+#endif
     for (;;)
       ; // Don't proceed, loop forever
   }
 
   displayInit(); //Start display and show beginning message
-
+#ifdef DEBUG
   DEBUG_PORT.begin(115200); //Start the serial monitor
   DEBUG_PORT.println("Starting GPS_DATA_LOGGER");
-
+#endif
   GPS_Serial.begin(9600, SERIAL_8N1, GPIO_NUM_34, GPIO_NUM_35); //Start the GPS serial
 
   // pinMode(15, INPUT_PULLUP); //Uncomment if you have issues getting SPI to work
@@ -453,7 +319,7 @@ void setup()
     display.clearDisplay();
     delay(1000);
   }
-
+#ifdef DEBUG
   DEBUG_PORT.print("SD Card Type: "); //just for debugging the sdcard
   if (cardType == CARD_MMC)
   {
@@ -471,15 +337,20 @@ void setup()
   {
     DEBUG_PORT.println("UNKNOWN");
   }
+#endif
 
   uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+#ifdef DEBUG
   DEBUG_PORT.printf("SD Card Size: %lluMB\n", cardSize);
+#endif
   //Here is where we write the CSV headder line and make our file
   if (cardSize)
   {
     sdIn = true;
+#ifdef DEBUG
     DEBUG_PORT.println("SDCARD IN");
     DEBUG_PORT.println(logFilePath);
+#endif
     writeFile(SD, logFilePath, "month,day,year,houre,minute,sec,lat,long,alt,speed(mph)\n");
   }
 }
